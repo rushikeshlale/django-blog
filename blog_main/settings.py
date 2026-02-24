c953f838f81298d0ca3e53fa3eb0bd407224ec8b
rushikeshlale <rushikeshlale1@gmail.com>
2026-02-24 12:00:20 +0530

    category crud functionlity

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blogs',
-    'assignments'
+    'assignments',
+    'crispy_forms',
+    'crispy_bootstrap4',
+    'dashboards',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +129,5 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR  /'media'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
